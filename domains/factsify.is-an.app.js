46f1d53414ffbe4421ed7e4eb43a4baf8683d401
Manu G <manusvathgurudath@gmail.com>
2024-05-29 12:55:51 -0700

    Update domains

---
+++ domains/factsify.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for turning your Spotify listening habits into a nutrition facts label',
+  domain: 'is-an.app',
+  subdomain: 'factsify',
+  owner: {
+    email: 'manugurudath@dysperse.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
